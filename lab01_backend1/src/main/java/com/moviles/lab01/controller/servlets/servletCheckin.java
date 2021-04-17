a858669a6749c98a18cbc2f9dc09fdcac4f8cedf
esoj1010 <56928967+esoj1010@users.noreply.github.com>
2021-04-17 12:21:59 -0600

    .
    .

---
+++ lab01_backend1/src/main/java/com/moviles/lab01/controller/servlets/servletCheckin.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.moviles.lab01.controller.servlets;
+
+import com.moviles.lab01.dao.services.serviceLogginUser;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jose
+ */
+public class servletCheckin extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private serviceLogginUser service = new serviceLogginUser();
+}
